41b5c20157ad03808b0a7e2eda25229bdf31543a
Jesse Wei <weihongji@qq.com>
2024-06-13 23:35:16 +0800

    report01: Add group type of 月事件id

--- sp.sql
+++ sp.sql
@@ -50,6 +50,20 @@ begin
 			and (v_load is null or test_load = v_load collate utf8mb4_general_ci)
 		group by year(start_time), weekofyear(start_time)
 		order by 1, 2;
+	elseif group_by = 'e' then -- 月事件id. 在一个月中，各月事件id出现的次数，若某事件id在一周内出现多次，则本周只计一次。
+		select `year`, `date`, moon_event_id, count(*) as 'count'
+		from (
+				select year(start_time) as 'year', date_format(start_time, '%Y-%m') as 'date', moon_event_id, weekofyear(start_time) as 'week'
+				from collect12
+				where (v_start is null or start_time >= v_start)
+					and (v_end is null or start_time < v_end)
+					and (v_task is null or test_task = v_task collate utf8mb4_general_ci)
+					and (v_load is null or test_load = v_load collate utf8mb4_general_ci)
+					and moon_event_id <> ''
+				group by year(start_time), date_format(start_time, '%Y-%m'), moon_event_id, weekofyear(start_time)
+			) x
+		group by `year`, `date`, moon_event_id
+		order by 1, 2, 3;
 	else -- total of all
 		select date_format(min(start_time), '%Y-%m-%d') as 'date', count(*) as 'count'
 		from collect12
